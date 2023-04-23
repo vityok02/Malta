ff2a721faca3f114a8b8ab4b73df443417f9b074
Віктор Липкань <lipkan02@gmail.com>
2023-04-23 13:10:39 +0300

    Fixed js bugs

--- script.js
+++ script.js
@@ -1,43 +1,44 @@
-$('.carousel').carousel({
+(function () {
+  $('.carousel').carousel({
     interval: 2000
-  })
+  });
 
-function openNav() {
-    document.getElementById("sidenav").style.width = "250px";
-}
-function closeNav() {
+  function openNav() {
+    $("#sidenav").width("250px");
+  }
+  function closeNav() {
     document.getElementById("sidenav").style.width = "0";
-}
+  }
 
-const animItems = document.querySelectorAll('._anim-items');
+  const animItems = $('._anim-items');
 
-if(animItems.length > 0){
-  window.addEventListener('scroll', animOnScroll());
-  function animOnScroll() {
-    for (let index = 0; index < animItems.length; index++) {
-      const animItem = animItems[index];
-      const animItemHeight = animItem.offsetHeight;
-      const animItemOffset = offset(animItem).top;
-      const animStart = 4;
+  if (animItems.length > 0) {
+    window.addEventListener('scroll', animOnScroll());
+    function animOnScroll() {
+      for (const animItem of animItems) {
+        const animItemHeight = animItem.offsetHeight;
+        const animItemOffset = offset(animItem).top;
+        const animStart = 4;
 
-      let animItemPoint = window.innerHeight - animItemHeight / animStart;
-      if(animItemHeight > window.innerHeight) {
-        animItemPoint = window.innerHeight - window.innerHeight / animStart;
-      }
+        let animItemPoint = window.innerHeight - animItemHeight / animStart;
+        if (animItemHeight > window.innerHeight) {
+          animItemPoint = window.innerHeight - window.innerHeight / animStart;
+        }
 
-      if((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
-        animItem.classList.add('_active');
-      } else {
-        animItem.classList.remove('_active');
+        if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+          animItem.classList.add('_active');
+        } else {
+          animItem.classList.remove('_active');
+        }
       }
     }
+    function offset(el) {
+      const rect = el.getBoundingClientRect(),
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+    }
+    animOnScroll();
   }
-  function offset(el) {
-    const rect = el.getBoundingClientRect(),
-      scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-      scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
-  }
-  animOnScroll();
-}
 
+})();
