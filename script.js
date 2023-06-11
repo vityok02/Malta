b0539015c07f12b94935c34268d992eb40476ddf
Віктор Липкань <lipkan02@gmail.com>
2023-06-11 21:00:27 +0300

    Adapted

--- script.js
+++ script.js
@@ -1,44 +1,14 @@
-(function () {
+const sidenav = $("#sidenav");
 
+function openNav() {
+  sidenav.width("250px");
+  document.getElementById('open-nav').style.display = "none";
+}
 
-  // function openNav() {
-  //   $("#sidenav").width("250px");
-  // }
-  // function closeNav() {
-  //   document.getElementById("sidenav").style.width = "0";
-  // }
-
-  // const animItems = $('._anim-items');
-
-  // if (animItems.length > 0) {
-  //   window.addEventListener('scroll', animOnScroll());
-  //   function animOnScroll() {
-  //     for (const animItem of animItems) {
-  //       const animItemHeight = animItem.offsetHeight;
-  //       const animItemOffset = offset(animItem).top;
-  //       const animStart = 4;
-
-  //       let animItemPoint = window.innerHeight - animItemHeight / animStart;
-  //       if (animItemHeight > window.innerHeight) {
-  //         animItemPoint = window.innerHeight - window.innerHeight / animStart;
-  //       }
-
-  //       if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
-  //         animItem.classList.add('_active');
-  //       } else {
-  //         animItem.classList.remove('_active');
-  //       }
-  //     }
-  //   }
-  //   function offset(el) {
-  //     const rect = el.getBoundingClientRect(),
-  //       scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-  //       scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  //     return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
-  //   }
-  //   animOnScroll();
-  // }
-
+function closeNav() {
+  document.getElementById("sidenav").style.width = "0";
+  document.getElementById('open-nav').style.display = "block";
+}
   
 const myCarouselElement = document.querySelector('#myCarousel')
 
@@ -46,4 +16,3 @@ const carousel = new bootstrap.Carousel(myCarouselElement, {
   interval: 2000,
   touch: false
 })
-})();
